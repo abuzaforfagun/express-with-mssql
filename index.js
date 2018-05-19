eaaf12cdd48205f03f4a7dcb40cd25eb1b375ff2
Fagun Rain <fagunrain@Faguns-MacBook-Pro.local>
2018-05-20 00:44:53 +0600

    Connect to mssql database

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var express = require("express");
 let app = express();
+const sql = require('mssql')
+let config = require('./const');
 
 app.use(express.json());
-
+let pool = "";
 app.listen(8000, ()=>{
-    console.log("I am ready to do operation!");
-})
+    
+    try {
+        pool = sql.connect(config)
+        console.log("I am ready to do operation!");
+    } catch (err) {
+        // ... error checks
+        console.log(err);
+    }
+});
