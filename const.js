69210c742e501ba2459829de2eb6ac6f7d8edfa6
Fagun Rain <fagunrain@Faguns-MacBook-Pro.local>
2018-05-20 00:44:43 +0600

    Add connection details

---
+++ const.js
@@ -0,0 +1,12 @@
+const config = {
+    user: 'DB_A1A66B_learning_admin',
+    password: 'abuzafor73azf',
+    server: 'sql5039.site4now.net', // You can use 'localhost\\instance' to connect to named instance
+    database: 'DB_A1A66B_learning',
+ 
+    options: {
+        encrypt: false // Use this if you're on Windows Azure
+    }
+}
+
+module.exports = config;
