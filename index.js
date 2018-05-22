49efb8b7803ff967b0cdf8ba8f1428ad582218b2
Fagun Rain <fagunrain@Faguns-MacBook-Pro.local>
2018-05-23 01:00:35 +0600

    Implement get one api call

--- index.js
+++ index.js
@@ -24,8 +24,7 @@ app.get("/api/customers", (req, res) => {
             let result1 = await pool.request()
                 .query('select * from customer')
                 
-            console.dir(result1)
-            res.send(result1);
+            res.send(result1.recordsets);
         
             // Stored procedure
             
@@ -41,6 +40,36 @@ app.get("/api/customers", (req, res) => {
     })()
      
     sql.on('error', err => {
+        res.sendStatus(400);
+    })
+
+});
+app.get("/api/customers/:id", (req, res) => {
+    (async function () {
+        try {
+            
+            let result1 = await pool.request()
+                .query(`select * from customer where id = ${req.params.id}`);
+                
+            console.log(result1)
+            res.send(result1.recordset);
+        
+            // Stored procedure
+            
+            // let result2 = await pool.request()
+            //     .input('input_parameter', sql.Int, value)
+            //     .output('output_parameter', sql.VarChar(50))
+            //     .execute('procedure_name')
+            
+            // console.dir(result2)
+        } catch (err) {
+            console.log(err);
+            res.sendStatus(400);
+        }
+    })()
+     
+    sql.on('error', err => {
+        console.log(err);
         res.sendStatus(400);
     })
 
